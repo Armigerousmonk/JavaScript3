d8cefeff7427ad4f08ae8f68a58ca533aa4c1d2b
Amirhossein <ahshadravan@gmail.com>
2021-01-05 18:31:27 +0330

    Ex3 is added

--- Week3/AmirHossein/homework/js-exercises/trivia-app/app.js
+++ Week3/AmirHossein/homework/js-exercises/trivia-app/app.js
@@ -16,37 +16,64 @@ const elFactory = (type, attributes, ...children) => {    // Function for creati
     return el
   };
 
-  const container = elFactory('div', {class: 'container'},
-    elFactory('h1',{}, 'Let\'s play some Trivia!'),
-    elFactory('h3', {}, 'Try your best to figure out the answer. If you really have no clue, click on the question to reveal the answer.'));
+const container = elFactory('div', {class: 'container'},
+  elFactory('h1',{}, 'Let\'s play some Trivia!'),
+  elFactory('h3', {}, 'Try your best to figure out the answer. If you really have no clue, click on the question to reveal the answer.'));
 
-console.log(container)
+const allPage = elFactory('div', {class: 'allpage'})
+allPage.appendChild(container)
+
 
 const list = elFactory('div', {id: 'list'})
-
-
-document.body.appendChild(container);
+document.body.appendChild(allPage);
 
 const url = 'https://opentdb.com/api.php?amount=5'
 
 function addTrivia(response) {
-
   response.results.forEach(element => {
-    console.log(element)
     const list = elFactory('div', {id: 'list'},
                   elFactory('p', {class: 'question'},
                     elFactory('p', {class: 'answer'})))
     const questions = list.querySelector('.question');
     const answer = list.querySelector('.answer');
+
     questions.innerHTML = element.question;
     answer.innerHTML = element.correct_answer;
     list.appendChild(questions);
     list.appendChild(answer);
     container.appendChild(list);
+    function openFunction() {
+
+      questions.addEventListener('click', function(){
+        answer.style.display = 'block';
+        closeFunction();
+      })
+    }
+    function closeFunction() {
+
+      questions.addEventListener('click', function(){
+          answer.style.display = 'none';
+          openFunction();
+        })
+      }
+      closeFunction();
+      document.addEventListener('click', function (event) {
+        const isClickInsideElement = container.contains(event.target);
+        if (!isClickInsideElement) {
+          answer.style.display = 'none'
+
+
+
+        };
+      });
+
+
 
 })
 
 }
+
+
 
 function getData1() {
   fetch(url)
